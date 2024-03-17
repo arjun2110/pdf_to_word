7e5f9902a0044696539cbb97f2d17376065c9df9
arjun2110 <arjunthumare@gmail.com>
2024-03-17 12:55:15 +0530

    final code

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ import streamlit as st
 import os
 from utils import pdf_to_word
 st.set_page_config("pdf to word converter",layout="wide")
-uploaded_file="./uploaded_file"
-converted_file="./converted_files"
+# uploaded_file=f"./pdf_to_word/uploaded_file"
+uploaded_file="C:/Users/ASUS/Documents/projects/pdf_to_word/pdf_to_word/uploaded_file"
+# converted_file=f"./pdf_to_word/converted_files"
+converted_file="C:/Users/ASUS/Documents/projects/pdf_to_word/pdf_to_word/converted_files/convertedfile.docx"
 file=st.file_uploader("Upload your file here",type=".pdf")
-if file:
+upload=st.button("Upload")
+if file and upload:
+    st.spinner()
     if not os.path.exists(uploaded_file):
         os.mkdir(uploaded_file)
 
@@ -13,4 +17,9 @@ if file:
     with open(file_path, 'wb') as f:
         f.write(file.getvalue())
     
-    pd
+    pdf_to_word(file_path, converted_file)
+    st.success("file converted successfully")
+    # st.download_button(file_path=conver)
+
+else:
+    st.warning('please upload dile to continue ')
--- utils.py
+++ utils.py
@@ -4,22 +4,22 @@ from docx import Document
 def pdf_to_word(pdf_path, word_path):
     # Open the PDF file
     with open(pdf_path, 'rb') as pdf_file:
-        pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
 
         # Create a Word document
         word_document = Document()
 
         # Iterate through each page and extract text
-        for page_number in range(pdf_reader.numPages):
-            page = pdf_reader.getPage(page_number)
-            text = page.extractText()
+        for page_number in range(len(pdf_reader.pages)):
+            page = pdf_reader.pages[page_number]
+            text = page.extract_text()
 
             # Add the extracted text to the Word document
-            word_document.add_paragraph(text)s
+            word_document.add_paragraph(text)
 
+        print(word_document)
         # Save the Word document
         word_document.save(word_path)
-        
 
 # # Example usage
 # pdf_file_path = 'input.pdf'
